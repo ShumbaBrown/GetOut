d9e0bb2b8bee36f6425d925c83f306cf0c540183
Candace Williams <candace.williams1@bison.howard.edu>
2018-11-03 17:16:56 -0700

    checks if door is within house, makes sure each house has at least 4 rooms, updates algorithm for changing rooms after a life is lost

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def floorPlan():
             roomLayout[key].append(value)
     #starts player in random room
     startingRoom = random.choice(list(roomLayout))
-    #ensures starting room is not the final room
-    while(startingRoom == len(roomLayout)):
+    #ensures starting room is not the final room and has at least 4 rooms
+    while(startingRoom == len(roomLayout) or int(startingRoom) < 4):
       startingRoom = random.choice(list(roomLayout))
     print('There are ', totalRooms, 'rooms in the House. Can you escape?')
     return startingRoom
@@ -24,39 +24,47 @@ def floorPlan():
 def roomManager(currentRoom, doorSelected):
     global gameOver
     global numLives
-    #sets the next room the sum of door and the current room
-    nextRoom = int(currentRoom) + int(doorSelected)
-    #loops player back to the same room
-    if(int(doorSelected) == int(currentRoom)):
-      nextRoom = 0
+    #checks if door selected is within the house
+    if(int(doorSelected) > len(roomLayout)):
+      print("This door does not exist. Please attempt again")
       return currentRoom
-    #checks if the next room is the last room or the exit
-    elif(nextRoom == len(roomLayout)):
-      gameOver = True
-      currentRoom = "Escaped"
-      print("You have", currentRoom)
     else:
-        #checks if a door selected leads to a room not in the House
-        #and all lives have been reduced
-        if(int(nextRoom) > len(roomLayout)):
-            if(numLives == 0):
-              gameOver = True
-              currentRoom = "Sunken Place"
-              print("You have entered in the", currentRoom)
-              nextRoom = 0
-              return currentRoom
-            else:
-              #deductes life and allows user to repeat the room
-              numLives = numLives - 1
-              currentRoom = int(nextRoom) - int(doorSelected)
-              return currentRoom
-
-
-        #sets the next room to the current room
-        else:
-          currentRoom = nextRoom
-          nextRoom = 0
-          return currentRoom
+        #sets the next room the sum of door and the current room
+      nextRoom = int(currentRoom) + int(doorSelected)
+      #loops player back to the same room
+      if(int(doorSelected) == int(currentRoom)):
+        nextRoom = 0
+        return currentRoom
+      #checks if the next room is the last room or the exit
+      elif(nextRoom == len(roomLayout)):
+        gameOver = True
+        currentRoom = "Escaped"
+        print("You have", currentRoom)
+      else:
+          #checks if a door selected leads to a room not in the House
+          #and all lives have been reduced
+          if(int(nextRoom) > len(roomLayout)):
+              if(numLives == 0):
+                gameOver = True
+                currentRoom = "Sunken Place"
+                print("You have entered in the", currentRoom)
+                nextRoom = 0
+                return currentRoom
+              else:
+                #deductes life
+                numLives = numLives - 1
+                #moves user to new room (value of door selected)
+                currentRoom = int(doorSelected)
+                #ensures the user isn't taken to the accept state
+                if (int(currentRoom) == len(roomLayout)):
+                    #moves user back to room 1
+                    currentRoom = 1
+                return currentRoom
+          #sets the next room to the current room
+          else:
+            currentRoom = nextRoom
+            nextRoom = 0
+            return currentRoom
 def main():
     currentRoom = floorPlan()
     while (not gameOver and numLives >= 0):
